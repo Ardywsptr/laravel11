0283be25fc611555d6c203a81f7a12f367d0ebdf
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-02 14:07:07 +0700

    feat(view): membuat beberapa view

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>halaman blog</title>
+</head>
+<body>
+  <a href="/">home</a>
+  <a href="/about">about</a>
+  <a href="/blog">blog</a>
+  <a href="/contact">contact</a>
+  <h1>halaman about</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus beatae deserunt tempore at eaque ex, minus doloribus eligendi est vitae suscipit ratione sit nisi cupiditate earum illum blanditiis quaerat voluptates natus iure ipsum! Quia est quos maiores saepe? Sapiente voluptate, placeat enim odit ut hic cupiditate odio sed exercitationem reiciendis labore ullam omnis quasi recusandae aliquam culpa consequuntur repellendus corporis incidunt. A odio, possimus dolorem eligendi doloremque omnis laudantium at voluptate aut quam. Sunt aspernatur numquam deserunt tempora itaque iste quod, explicabo fugit vero temporibus, nam quam consectetur odit, provident dicta. Quisquam, ea autem laborum vero ullam cupiditate possimus fugiat sapiente ad temporibus nostrum exercitationem non necessitatibus eius eveniet voluptatibus eligendi numquam quidem minima maxime, labore nemo! Eius eligendi, illum error dolor quasi excepturi corrupti tempore quo voluptas consectetur laboriosam voluptatum ab cum odio? Inventore ipsum nostrum quod natus accusamus, id ad unde aut architecto labore facere sapiente nobis neque aspernatur blanditiis tempora enim tenetur consectetur? Consequatur, vero. Ipsam nisi autem nihil cupiditate repudiandae fugiat, aspernatur, veritatis tempora totam, nam enim repellendus. Quis vitae officia tempore nesciunt doloremque, et amet harum libero dolore ratione id explicabo eos laudantium esse ipsa unde quasi beatae quisquam officiis? Amet consectetur pariatur molestiae optio?</p>
+</body>
+</html>
