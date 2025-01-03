20ab1016c0cc97a8b69d5e65a6881f67436941da
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-03 15:58:39 +0700

    feat(blade component): menggunakan komponen dan layout pada setiap halaman

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>halaman about</title>
-</head>
-<body>
-  <a href="/">home</a>
-  <a href="/about">about</a>
-  <a href="/blog">blog</a>
-  <a href="/contact">contact</a>
-  <h1>halaman about</h1>
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Ini adalah halaman about</h3>
   <h3>owner : {{ $name }}</h3>
   <img src="img/logo.png" alt="logo">
-</body>
-</html>
+</x-layout>
+
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>halaman blog</title>
-</head>
-<body>
-  <a href="/">home</a>
-  <a href="/about">about</a>
-  <a href="/blog">blog</a>
-  <a href="/contact">contact</a>
-  <h1>halaman contact</h1>
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Ini adalah halaman contact</h3>
   <ul>
     <li>instagram: <a href="https://instagram.com/ardywsptr_">@ardywsptr_</a></li>
     <li>x: <a href="https://x.com/ardywsptr_">@ardywsptr_</a></li>
     <li>youtube: <a href="https://youtube.com/ardywsptr_">@ardywsptr_</a></li>
   </ul>
-</html>
+</x-layout>
