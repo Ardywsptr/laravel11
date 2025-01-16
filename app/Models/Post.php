cfa35ac3b2afd97fa6d200bd86bcc67ac9f699d7
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-16 12:03:19 +0700

    refactor: menambahkan mass assigment
    mass assigment berguna ketika kita ingin menambahkan data ke dalam database secara langsung

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 // NOTED: MEMBUAT CLASS DENGAN STATIC METHOD UNTUK DATA POST (AGAR TIDAK REDUNDANT)
 class Post extends Model{
 
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+
 }
 
 ?>
