4082903ec1e5ff3929f56d71f4f6261de2dbf441
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-17 20:24:16 +0700

    refactor: merubah url id dengan username

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
     <article class="max-w-screen-md py-8 border-b border-gray-300">
       <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
       <div class="text-base text-gray-500">
-        <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
+        <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
       </div>
       <p class="my-4 font-light">{{ $post['body'] }}</p>
       <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
