f66c2d50293e8f155ae85cd89df5fe417551714b
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-18 08:55:12 +0700

    feat(post): menambahkan container view u/ category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,8 +6,11 @@
       <a href="/posts/{{ $post['slug'] }}">
         <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900 hover:underline">{{ $post['title'] }}</h2>
       </a>
-      <div class="text-base text-gray-500">
-        <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
+      <div>
+        By
+        <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a> 
+        In
+        <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">Category</a> | {{ $post->created_at->diffForHumans() }}
       </div>
       <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
       <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
