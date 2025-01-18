196018f5e972ba0707500c778d3da2e633905484
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-18 20:04:33 +0700

    feat: database seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Category::factory(5)->create();
+
+        $categories = [ 'Frontend Web', 'Backend Web', 'UI UX', 'CI CD', 'Testing' ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+            ]);
+        }
+    }
+}
