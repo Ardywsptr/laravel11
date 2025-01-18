f8c7ff157646bc1631c61238d5d7d7e3d9de8aa7
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-18 10:54:40 +0700

    feat: view categories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
         By
         <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a> 
         In
-        <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">Category</a> | {{ $post->created_at->diffForHumans() }}
+        <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a> | {{ $post->created_at->diffForHumans() }}
       </div>
       <p class="my-4 font-light">{{ $post['body'] }}</p>
       <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
         By
         <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a> 
         In
-        <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">Category</a> | {{ $post->created_at->diffForHumans() }}
+        <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a> | {{ $post->created_at->diffForHumans() }}
       </div>
       <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
       <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
