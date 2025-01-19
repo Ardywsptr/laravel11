7dbbb9bb64e9c96476ffa0e99f25ee542068582c
Ardy wirasaputra <ardywsptr1@gmail.com>
2025-01-19 20:30:53 +0700

    style: merubah bg category secara dinamis

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,23 +3,10 @@
     <div class="max-w-screen-xl px-4 py-4 mx-auto lg:py-8 lg:px-0">
         <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
           @foreach ($posts as $post)
-            {{-- <article class="max-w-screen-md py-8 border-b border-gray-300">
-              <a href="/posts/{{ $post['slug'] }}">
-                <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900 hover:underline">{{ $post['title'] }}</h2>
-              </a>
-              <div>
-                By
-                <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a> 
-                In
-                <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a> | {{ $post->created_at->diffForHumans() }}
-              </div>
-              <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
-              <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-            </article> --}}
             <article class="p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex items-center justify-between mb-5 text-gray-500">
                   <a href="/categories/{{ $post->category->slug }}">
-                    <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800 hover:shadow-sm hover:bg-primary-200">
+                    <span class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-{{ $post->category->color }}-200 dark:text-primary-800 hover:shadow-sm hover:bg-{{ $post->category->color }}-200">
                         {{ $post->category->name }}
                     </span>
                   </a>
